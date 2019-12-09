ab22294a13041ac90a1f559b89c0f2cfbc6e232c
Paul Wilson <54870992+wilsonpg@users.noreply.github.com>
2019-12-09 00:04:54 -0500

    final changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Dialogs } from '@ionic-native/dialogs/ngx';
 
-let number = 1;
-// Math.floor(Math.random() * 10) + 1;
+let number = Math.floor(Math.random() * 10) + 1;
+let guesses = 0;
 
 @Component({
   selector: 'app-tab2',
@@ -16,7 +16,7 @@ export class Tab2Page {
   buttonOne() {
     const clicked = 1;
     if(clicked === number){
-      this.dialogs.confirm('Play Again?', 'You won!', ['Yes', 'No'])
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
         .then((buttonIndex) => {
           if (buttonIndex === 1) {
             location.reload();
@@ -25,7 +25,144 @@ export class Tab2Page {
         .catch(e => console.log('Error displaying dialog', e));
     }
     else{
-      alert(`Incorrect, try again!`);
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonTwo() {
+    const clicked = 2;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonThree() {
+    const clicked = 3;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonFour() {
+    const clicked = 4;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonFive() {
+    const clicked = 5;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonSix() {
+    const clicked = 6;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonSeven() {
+    const clicked = 7;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonEight() {
+    const clicked = 8;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
+    }
+  }
+
+  buttonNine() {
+    const clicked = 9;
+    if(clicked === number){
+      this.dialogs.confirm('Play Again?', 'You won by selecting' + number + 'after' + guesses + 'guesses!', ['Yes', 'No'])
+        .then((buttonIndex) => {
+          if (buttonIndex === 1) {
+            location.reload();
+          }
+        })
+        .catch(e => console.log('Error displaying dialog', e));
+    }
+    else{
+      this.dialogs.alert(`Incorrect, try again!`);
+      guesses += 1;
     }
   }
 }
